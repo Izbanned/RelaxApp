86ba777fc1ee9301f023538c7fafe2fe0867271a
Dias <201750@astanait.edu.kz>
2022-04-04 12:17:17 +0600

    Initial Commit

---
+++ RelaxApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  RelaxApp
+//
+//  Created by Dias Karimov on 10.02.2022.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    var player: AVAudioPlayer!
+    @IBAction func pressedButtons(_ sender: UIButton) {
+         playSound(soundName: sender.currentTitle! )
+    }
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
