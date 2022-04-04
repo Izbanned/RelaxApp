0ec1b0a33697cfbbdea882b9bdffcd50518a5c8d
Dias <201750@astanait.edu.kz>
2022-04-04 17:24:33 +0600

    little fixes

--- RelaxApp/ViewController.swift
+++ RelaxApp/ViewController.swift
@@ -23,7 +23,6 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
     }
 }
 
